09c327642982ecc3db65e2c4ae81ac06233df990
Vlad Marlo <vlad.marlo@ya.ru>
2023-04-30 00:28:40 +0300

    added marshalling unmarshalling of time interval type

--- internal/model/requests.go
+++ internal/model/requests.go
@@ -1,15 +1,17 @@
 package model
 
-type Courier struct {
-	CourierID    int      `json:"courier_id"`
-	CourierType  string   `json:"courier_type"`
-	Regions      []int    `json:"regions"`
-	WorkingHours []string `json:"working_hours"`
-}
+const (
+	FootCourierType = "FOOT"
+	BikeCourierType = "BIKE"
+	AutoCourierType = "AUTO"
+)
 
-type CouriersCreateRequest []struct {
-	CourierID    int      `json:"courier_id"`
-	CourierType  string   `json:"courier_type"`
-	Regions      []int    `json:"regions"`
-	WorkingHours []string `json:"working_hours"`
-}
+type (
+	Courier struct {
+		CourierID    int             `json:"courier_id"`
+		CourierType  string          `json:"courier_type"`
+		Regions      []int           `json:"regions"`
+		WorkingHours []*TimeInterval `json:"working_hours"`
+	}
+	CouriersCreateRequest []Courier
+)
--- internal/model/working_hours.go
+++ internal/model/working_hours.go
@@ -1,15 +1,16 @@
 package model
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"strconv"
 	"strings"
+	"time"
 )
 
 var (
 	ErrBadWorkingHours = errors.New("working hours must be in HH:MM-HH:MM format")
-	//workingHoursRegexp = regexp.MustCompile("^\\d+:\\d+-\\d+:\\d+$")
 )
 
 const (
@@ -31,26 +32,26 @@ type (
 	}
 )
 
-// ParseWorkTime parses time from raw string.
+// ParseWorkTime parses time in HH:MM format from string.
 //
 // Time must be presented in HH:MM format.
 //
 // Only error that can be returned from this function is ErrBadWorkingHours of child
 // error of itself.
 func ParseWorkTime(raw string) (w Time, err error) {
-	time := strings.Split(raw, ":")
-	if len(time) != 2 {
-		return Time{}, ErrBadWorkingHours
+	t := strings.Split(raw, ":")
+	if len(t) != 2 {
+		return w, ErrBadWorkingHours
 	}
 	var hour, minute int
-	if hour, err = strconv.Atoi(time[0]); err != nil {
+	if hour, err = strconv.Atoi(t[0]); err != nil {
 		return w, fmt.Errorf("%w: err while parsing hour: %v", ErrBadWorkingHours, err)
 	}
 	if hour > maxHourValue {
 		return w, ErrBadWorkingHours
 	}
 
-	if minute, err = strconv.Atoi(time[1]); err != nil {
+	if minute, err = strconv.Atoi(t[1]); err != nil {
 		return w, fmt.Errorf("%w: err while parsing minute: %v", ErrBadWorkingHours, err)
 	}
 	if minute > maxMinuteValue {
@@ -87,18 +88,89 @@ func ParseWorkingHours(raw string) (h *TimeInterval, err error) {
 	return h, nil
 }
 
-// In return is time in interval or not.
-func (h *TimeInterval) In(time Time) bool {
-	if h.reverse {
-		return (h.start.Less(time) && !(time.Less(h.end))) || time.Less(h.end)
+// String returns string representation of time interval.
+//
+// String representation of time interval always parsable.
+//
+// Time interval is returned in HH:MM-HH:MM format.
+func (h *TimeInterval) String() string {
+	return fmt.Sprintf("%s-%s", h.start, h.end)
+}
+
+// TimeIn return is time in interval or not.
+func (h *TimeInterval) TimeIn(t time.Time) bool {
+	return Time{
+		hour:   uint8(t.Hour()),
+		minute: uint8(t.Minute()),
+	}.In(h)
+}
+
+func (h *TimeInterval) MarshalJSON() ([]byte, error) {
+	return json.Marshal(h.String())
+}
+
+func (h *TimeInterval) UnmarshalJSON(data []byte) error {
+	var raw string
+	if err := json.Unmarshal(data, &raw); err != nil {
+		return err
 	}
-	return time.Less(h.end) && h.start.Less(time)
+	interval, err := ParseWorkingHours(raw)
+	if err != nil {
+		return err
+	}
+	*h = *interval
+	return nil
+}
+
+// Start return start of time interval.
+//
+// If pointer receiver is nil object then will be returned zero time.
+func (h *TimeInterval) Start() (t Time) {
+	if h == nil {
+		return
+	}
+	return h.start
+}
+
+// End return end of time interval.
+//
+// If pointer receiver is nil object then will be returned zero time.
+func (h *TimeInterval) End() (t Time) {
+	if h == nil {
+		return
+	}
+	return h.end
+}
+
+// In return is time in interval or not.
+func (t Time) In(h *TimeInterval) bool {
+	if h.reverse {
+		return (h.start.Less(t) && !(t.Less(h.end))) || t.Less(h.end)
+	}
+	return t.Less(h.end) && h.start.Less(t)
+}
+
+// String returns string representation of time.
+//
+// Time will be in format HH:MM and must be parsable back to Time object from string.
+func (t Time) String() string {
+	return fmt.Sprintf("%d:%d", t.hour, t.minute)
+}
+
+// Hour is hour accessor.
+func (t Time) Hour() uint8 {
+	return t.hour
+}
+
+// Minute is minute accessor.
+func (t Time) Minute() uint8 {
+	return t.minute
 }
 
 // Less is helper comparator function.
-func (h Time) Less(other Time) bool {
-	if h.hour == other.hour {
-		return h.minute < other.minute
+func (t Time) Less(other Time) bool {
+	if t.hour == other.hour {
+		return t.minute < other.minute
 	}
-	return h.hour < other.hour
+	return t.hour < other.hour
 }
